ad565ce1dbab9c674a335faa1ee1db659d6d165c
ChainHokes <k.nazar2003@gmail.com>
2022-11-14 17:47:26 +0300

    base commit

---
+++ backend/backend/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+] + staticfiles_urlpatterns()
